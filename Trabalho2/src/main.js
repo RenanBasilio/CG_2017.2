4255569b5215a28bee1c0cfe4fc744c5944a533a
Renan Basilio <renanbasilio@poli.ufrj.br>
2017-09-29 20:01:48 -0300

    Start Trabalho 2.

---
+++ Trabalho2/src/main.js
@@ -0,0 +1,7 @@
+function setup(){
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.01, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
